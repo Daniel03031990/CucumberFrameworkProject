2f4ec95be775205d1379e0da769a7c797bee98c8
Daniel03031990 <grachiladaniel@gmail.com>
2024-10-20 11:24:01 +0100

    1stcommit

---
+++ src/test/java/utils/Constants.java
@@ -0,0 +1,9 @@
+package utils;
+
+public class Constants {
+    public static final String CONFIG_FILE_PATH=System.getProperty("user.dir")+"\\src\\test\\resources\\config\\config.properties";
+    public static final String SCREENSHOT_FILEPATH = System.getProperty("user.dir") + "\\screenshot";
+    public static final String EXCEL_FILE_PATH= System.getProperty("user.dir") + "\\src\\test\\resources\\testdata\\testExcelFile.xlsx";
+
+    //public static String configReaderPath;
+}
