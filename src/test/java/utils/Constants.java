e602d62aba1808deaacaa66c5e3ef5264521d29e
Daniel03031990 <grachiladaniel@gmail.com>
2024-11-20 14:06:58 +0000

    Changes added.new changes added

--- src/test/java/utils/Constants.java
+++ src/test/java/utils/Constants.java
@@ -1,7 +1,7 @@
 package utils;
 
 public class Constants {
-    public static final String CONFIG_FILE_PATH=System.getProperty("user.dir")+"//src//test//resources//config//config.properties";
+    public static final String CONFIG_FILE_PATH=System.getProperty("user.dir") + "//src//test//resources//config//config.properties";
     public static final String SCREENSHOT_FILEPATH = System.getProperty("user.dir") + "//screenshot";
     public static final String EXCEL_FILE_PATH= System.getProperty("user.dir") + "//src//test//resources//testdata//Book1.xlsx";
 
